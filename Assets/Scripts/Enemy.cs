a4cd1553e133300761c5a875bb2ee06ab360055a
Khanh Hoang Quoc <khanh.hoangquoc91@gmail.com>
2026-06-05 16:53:45 +0700

    * Setup enemy class

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,5 +2,28 @@ using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour
 {
+    [SerializeField] private Stats stats;
+	private Health health;
 
+	private void Awake()
+	{
+		health = new Health(stats.health);
+		health.onDie += OnDie;
+	}
+
+	private void OnDestroy()
+	{
+		health.onDie -= OnDie;
+	}
+
+	public virtual void TakeDamage(int amount)
+	{
+		health.TakeDamage(amount);
+	}
+
+	protected virtual void OnDie()
+	{
+		Debug.Log("Enemy died");
+		gameObject.SetActive(false);
+	}
 }
